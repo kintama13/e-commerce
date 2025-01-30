d81277e256cf3952d8cd928854a0d029ad5bbfef
kintama13 <11201059@student.itk.ac.id>
2025-01-30 13:58:30 +0800

    product filter and error ini 'is_featured'

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -11,7 +11,7 @@
                 @foreach ($categories as $category)
                     <li class="mb-4" wire:key="{{$category->id}}">
                         <label for="{{$category->slug}}" class="flex items-center dark:text-gray-400 ">
-                        <input type="checkbox" id="{{$category->slug}}" value="{{$category->id}}" class="w-4 h-4 mr-2">
+                        <input type="checkbox" wire:model.live="selected_categories" id="{{$category->slug}}" value="{{$category->id}}" class="w-4 h-4 mr-2">
                         <span class="text-lg">{{$category->name}}</span>
                         </label>
                     </li>
@@ -28,7 +28,7 @@
                 @foreach ($brands as $brand)
                     <li class="mb-4" wire:key="{{$brand->id}}">
                         <label for="{{$brand->slug}}" class="flex items-center dark:text-gray-300">
-                        <input type="checkbox" id="{{$brand->slug}}" value="{{$brand->id}}" class="w-4 h-4 mr-2">
+                        <input type="checkbox" wire:model.live="selected_brands" id="{{$brand->slug}}" value="{{$brand->id}}" class="w-4 h-4 mr-2">
                         <span class="text-lg dark:text-gray-400">{{$brand->name}}</span>
                         </label>
                     </li>
@@ -40,15 +40,15 @@
               <h2 class="text-2xl font-bold dark:text-gray-400">Product Status</h2>
               <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
               <ul>
-                <li class="mb-4">
-                  <label for="" class="flex items-center dark:text-gray-300">
-                    <input type="checkbox" class="w-4 h-4 mr-2">
-                    <span class="text-lg dark:text-gray-400">In Stock</span>
+                {{-- <li class="mb-4">
+                  <label for="featured" class="flex items-center dark:text-gray-300">
+                    <input type="checkbox" id="featured" wire:model.live="featured" value="1" class="w-4 h-4 mr-2">
+                    <span class="text-lg dark:text-gray-400">Is Featured</span>
                   </label>
-                </li>
+                </li> --}}
                 <li class="mb-4">
-                  <label for="" class="flex items-center dark:text-gray-300">
-                    <input type="checkbox" class="w-4 h-4 mr-2">
+                  <label for="on_sale" class="flex items-center dark:text-gray-300">
+                    <input type="checkbox" wire:model.live="on_sale" id="on_sale" class="w-4 h-4 mr-2">
                     <span class="text-lg dark:text-gray-400">On Sale</span>
                   </label>
                 </li>
@@ -59,10 +59,13 @@
               <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
               <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
               <div>
-                <input type="range" class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer" max="500000" value="100000" step="100000">
+                <div class="font-semibold">
+                    {{Number::currency($price_range, 'IDR')}}
+                </div>
+                <input type="range" wire:model.live="price_range" class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer" max="30000000" value="300000" step="100000">
                 <div class="flex justify-between ">
-                  <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                  <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
+                  <span class="inline-block text-lg font-bold text-blue-400 ">{{Number::currency(150, 'IDR')}}</span>
+                  <span class="inline-block text-lg font-bold text-blue-400 ">{{Number::currency(30000, 'IDR')}}</span>
                 </div>
               </div>
             </div>
