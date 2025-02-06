9b4c493e026f50ee5d32dfe1a0f334db721decd4
kintama13 <11201059@student.itk.ac.id>
2025-02-06 15:30:58 +0800

    register and login page done

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -90,7 +90,7 @@
               </span>
             </div>
             @if ($cart_items)
-                <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                <a href="/checkout" class="bg-blue-500 block text-center text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</a href="">
             @endif
           </div>
         </div>
